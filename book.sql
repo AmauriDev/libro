95ecab8f0811ef92196f32261e7cc383fee04542
Amauri <dsrsoft2010@hotmail.com>
2022-09-22 20:48:14 -0400

    correccion en el orden del sql

--- book.sql
+++ book.sql
@@ -2,30 +2,30 @@ CREATE DATABASE book;
 
 
 CREATE TABLE author(
-autor_id NOT NULL UNSIGNED SMALLINT AUTO_INCREMENT,
-author_name NOT NULL VARCHAR(15),
-author_last_name NOT NULL VARCHAR(20),
+author_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+author_name VARCHAR(15) NOT NULL ,
+author_last_name VARCHAR(20) NOT NULL ,
 PRIMARY KEY(author_id)
 );
 
 CREATE TABLE editorial(
-editorial_id NOT NULL UNSIGNED SMALLINT AUTO_INCREMENT,
-editorial_name NOT NULL VARCHAR(20),
-editorial_web NOT NULL VARCHAR(100),
+editorial_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+editorial_name VARCHAR(20) NOT NULL,
+editorial_web VARCHAR(100) NOT NULL,
 PRIMARY KEY(editorial_id)
 );
 
 CREATE TABLE books(
-book_id NOT NULL UNSIGNED SMALLINT,
-autor_id NOT NULL UNSIGNED SMALLINT,
-editorial_id NOT NULL UNSIGNED SMALLINT,
-book_title NOT NULL VARCHAR(50),
-book_isbn NOT NULL VARCHAR(12),
-book_description NOT NULL TEXT,
-book_chapter NOT NULL SMALLINT,
-book_pages NOT NULL SMALLINT,
-book_cover NOT NULL CHAR(255),
-book_type_text NOT NULL VARCHAR(100),
-book_date NOT NULL DATE,
+book_id SMALLINT UNSIGNED NOT NULL,
+autor_id SMALLINT UNSIGNED NOT NULL,
+editorial_id SMALLINT UNSIGNED NOT NULL,
+book_title VARCHAR(50) NOT NULL,
+book_isbn VARCHAR(12) NOT NULL,
+book_description TEXT NOT NULL,
+book_chapter SMALLINT UNSIGNED NOT NULL ,
+book_pages SMALLINT UNSIGNED NOT NULL ,
+book_cover CHAR(255) NOT NULL,
+book_type_text VARCHAR(100) NOT NULL,
+book_date DATE NOT NULL,
 PRIMARY KEY(book_id)
 );
