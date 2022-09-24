d768c1175a1b77e97cb4b324069bb19383f266ba
Amauri <dsrsoft2010@hotmail.com>
2022-09-23 20:22:20 -0400

    Archivo php para guardat datos del author en la base de datos

--- author.php
+++ author.php
@@ -1,4 +1,24 @@
 <?php
 include('funciones.php');
+// creamos una coneccion 
+$con = coneccion();
+$db = 'book';
+
+//Obtenemos los datos del formulario 
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$nacinalidad = $_POST['nacionalidad'];
+$profesion = $_POST['profesion'];
+
+//Vamos a crear la oracion de la consulta 
+$insert = "INSERT INTO author VALUES ('', '$nombre', '$apellido', '$nacionalidad', '$profesion')";
+//Vamos a enviar la consulta 
+$inserted = mysqli_query($con, $insert);
+if($inserted){
+	echo 'Lo datos fueron guardado con exito';
+}else
+{
+	echo 'Error al guardar los datos';
+}
 
 ?>
