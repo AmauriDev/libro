25dbc9768ad99b1a68d75a8acd7be0bbe51f8414
Amauri <dsrsoft2010@hotmail.com>
2022-09-24 20:55:47 -0400

    Visualisador de libros

---
+++ php/verlibros.php
@@ -0,0 +1,20 @@
+<?php 
+include('funciones.php');
+$con = coneccion();
+$db = 'book';
+mysqli_select_db($con, $db);
+$select = "SELECT book_title, author_name, editorial_name, editorial_web FROM books, author, editorial WHERE books.autor_id = author.author_id AND books.editorial_id = editorial.editorial_id";
+$query = mysqli_query($con, $select);
+if($query){
+	$num = mysqli_num_rows($query);
+	if($num >= 1){
+		while($row = mysqli_fetch_assoc($query)){
+			echo '<b>Titulo</b> '.$row['book_title'].' <b>Autor</b> '.$row['author_name'].' <b>Editorial</b> <a href=\''.$row['editorial_web'].'\'>'. $row['editorial_name'].'</a></br>';
+		}
+	}else{
+		echo 'No hay resultados';
+	}
+}else{
+	echo 'Consulta no enviada';
+}
+?>
